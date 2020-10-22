ce3c99ec506e7dea2b3189eb9a97d05ec8febe7d
HwnagYoungJun <dudghkd4512@gmail.com>
2020-10-22 15:00:11 +0900

    S03P31D201-1 | init

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+	<div id="app">
+		<router-view />
+	</div>
+</template>
+
+<style lang="scss">
+@import './assets/css/reset.css';
+@import './assets/css/common.css';
+
+#app {
+	min-height: 100vh;
+}
+</style>
