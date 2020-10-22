92826b12909682f51b8b7d15734c5a4077b1880e
jwdo <ehwjddn279@naver.com>
2020-10-22 13:37:01 +0900

    tour

--- backend/gumi/tour/urls.py
+++ backend/gumi/tour/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 app_name = 'tour'
 urlpatterns = [
-    path('<int:course_id>/',views.Course.as_view(),name='course'),
-    path('place/<int:place_id>/',views.List.as_view(),name='list'),
-    path('place/<int:place_id>/mission/',views.Mission.as_view(),name='mission'),
-    path('place/<int:user_id>/custom/',views.Custom.as_view(),name='custom'),
-    # path('place/<int:user_id>/custom/<int:mission_id>/',views.Custominfo.as_view(),name='custominfo')
+    path('<int:course_id>/',views.CourseView.as_view(),name='course'),
+    path('place/<int:place_id>/',views.ListView.as_view(),name='list'),
+    path('place/<int:place_id>/mission/',views.MissionView.as_view(),name='mission'),
+    path('place/<int:user_id>/custom/',views.CustomView.as_view(),name='custom'),
+    path('place/<int:user_id>/custom/<int:mission_id>/',views.CustomInfoView.as_view(),name='custominfo')
 ]
