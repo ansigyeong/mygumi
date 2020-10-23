76a316b57128b3054185a8e6ea20632b14726995
안시경 <jeongseok95@naver.com>
2020-10-23 13:58:54 +0900

    S03P31D201-5 | Bottom Navigation

--- frontend/src/components/common/BottomNav.vue
+++ frontend/src/components/common/BottomNav.vue
@@ -1,19 +1,19 @@
 <template>
 	<v-bottom-navigation :value="value" color="primary" grow>
 		<v-btn>
-			<span>HOME</span>
+			<span>홈</span>
 
 			<v-icon>mdi-home</v-icon>
 		</v-btn>
 
 		<v-btn>
-			<span>GO</span>
+			<span>여행 시작하기</span>
 
 			<v-icon>mdi-map-marker</v-icon>
 		</v-btn>
 
 		<v-btn>
-			<span>PROFILE</span>
+			<span>프로필</span>
 
 			<v-icon>mdi-account</v-icon>
 		</v-btn>
