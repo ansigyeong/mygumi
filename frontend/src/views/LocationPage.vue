cc3b972095a308c21b17f4d4491921d39a03f6f8
안시경 <jeongseok95@naver.com>
2020-10-26 15:09:49 +0900

    S03P31D201-18 | 지도

--- frontend/src/views/LocationPage.vue
+++ frontend/src/views/LocationPage.vue
@@ -16,12 +16,8 @@
   - 작성일
 - 후기 작성 버튼 (-> __Review Create Page__)
 -->
-<script
-	type="text/javascript"
-	src="//dapi.kakao.com/v2/maps/sdk.js?appkey=ee44fe7d97581683a916ae26e90c5ba1"
-></script>
 <template>
-	<div>
+	<div class="temp">
 		<!-- 여행지 정보 -->
 		<v-card class="mx-auto">
 			<v-img
@@ -106,7 +102,6 @@
 			<v-card-title>
 				단풍 구경은 금오산에서!
 			</v-card-title>
-
 			<v-card-subtitle>
 				1,000 miles of wonder
 			</v-card-subtitle>
@@ -116,17 +111,94 @@
 
 <script>
 export default {
-	data() {
-		return {
-			container: document.getElementById('map'),
-			options: {
+	// data() {
+	// 	return {
+	// 		container: document.getElementById('map'),
+	// 		options: {
+	// 			center: new kakao.maps.LatLng(33.450701, 126.570667),
+	// 			level: 3,
+	// 		},
+	// 		map: new kakao.maps.Map(container, options),
+	// 	};
+	// },
+	computed: {
+		kakaoKey() {
+			return process.env.VUE_APP_KAKAO_KEY;
+		},
+		kakaoRESTAPI() {
+			return process.env.VUE_APP_KAKAO_REST_API_KEY;
+		},
+	},
+	mounted() {
+		// console.log(this.kakaoKey);
+		if (window.kakao && window.kakao.maps) {
+			this.initMap();
+			console.log('여기');
+		} else {
+			const script = document.createElement('script');
+			console.log('저기');
+			/* global kakao */
+			script.onload = () => kakao.maps.load(this.initMap);
+			script.src = `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${this.kakaoKey}`;
+			document.head.appendChild(script);
+		}
+	},
+	methods: {
+		initMap() {
+			var infowindow = new kakao.maps.InfoWindow({ zIndex: 1 });
+
+			function displayMarker(place) {
+				// 마커를 생성하고 지도에 표시합니다
+				var marker = new kakao.maps.Marker({
+					map: map,
+					position: new kakao.maps.LatLng(place.y, place.x),
+				});
+
+				// 마커에 클릭이벤트를 등록합니다
+				kakao.maps.event.addListener(marker, 'click', function() {
+					// 마커를 클릭하면 장소명이 인포윈도우에 표출됩니다
+					infowindow.setContent(
+						'<div style="padding:5px;font-size:12px;">' +
+							place.place_name +
+							'</div>',
+					);
+					infowindow.open(map, marker);
+				});
+			}
+			function placesSearchCB(data, status) {
+				if (status === kakao.maps.services.Status.OK) {
+					// 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+					// LatLngBounds 객체에 좌표를 추가합니다
+					var bounds = new kakao.maps.LatLngBounds();
+
+					for (var i = 0; i < data.length; i++) {
+						displayMarker(data[i]);
+						bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+					}
+
+					// 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+					map.setBounds(bounds);
+				}
+			}
+			var container = document.getElementById('map');
+			var options = {
 				center: new kakao.maps.LatLng(33.450701, 126.570667),
 				level: 3,
-			},
-			map: new kakao.maps.Map(container, options),
-		};
+			};
+
+			var map = new kakao.maps.Map(container, options);
+			// 장소 검색 객체를 생성합니다
+			var ps = new kakao.maps.services.Places();
+			console.log(ps);
+			// 키워드로 장소를 검색합니다
+			ps.keywordSearch('금오산', placesSearchCB);
+			map.setMapTypeId(kakao.maps.MapTypeId.HYBRID);
+		},
 	},
+	// async keywordToLoaction() {
+
+	// }
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
