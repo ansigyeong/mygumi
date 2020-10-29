bb513ae18f63399411a1a27b3d87d0d87547eba2
Lunarus00 <moongur1900@naver.com>
2020-10-29 14:58:53 +0900

    aws 확인

--- backend/gumi/gumi/urls.py
+++ backend/gumi/gumi/urls.py
@@ -13,9 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework import routers, permissions
+
+# swagger
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# swagger 정보 설정, 관련 엔드포인트 추가
+# swagger 엔드포인트는 DEBUG Mode에서만 노출
+schema_view = get_schema_view(
+    openapi.Info(
+        title="GumiTour API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include('accounts.urls')),
     path('tour/', include('tour.urls')),
+    path('review/', include('review.urls')),
+
+    # swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
+
+    # rest-auth
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/signup/', include('rest_auth.registration.urls')),
 ]
