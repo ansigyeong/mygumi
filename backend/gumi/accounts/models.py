0e8342bd126116ee11e3af2835cd4fbb641419e1
Lunarus00 <moongur1900@naver.com>
2020-11-05 14:42:55 +0900

    S03P31D201-45 | 장소 정보 테이블 수정(코스명, 운영시간 추가) 및 image 파일 저장 위치 재설정(투어, 프로필)

--- backend/gumi/accounts/models.py
+++ backend/gumi/accounts/models.py
@@ -8,7 +8,7 @@ User = settings.AUTH_USER_MODEL
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=50)
-    profile_image = models.ImageField(upload_to='media/profile/', default='media/default.jpg')
+    profile_image = models.ImageField(upload_to='profile/', default='media/default.jpg')
 
 class VisitCheck(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
