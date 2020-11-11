c828f9e11c253f9dc62175f207a524418644279e
kei9641 <kei9641@naver.com>
2020-11-11 21:51:14 +0900

    일정 생성

--- frontend/src/api/schedule.js
+++ frontend/src/api/schedule.js
@@ -1,5 +1,9 @@
 import { auth } from '@/api/index';
 
 export function scheduleList(userId) {
-	return auth.get(`schedule/${userId}`);
+	return auth.get(`schedule/${userId}/`);
+}
+
+export function addSchedule(userId, scheduleData) {
+	return auth.post(`schedule/${userId}/`, scheduleData);
 }
